a612d934f9528a69b1e79c3f49d8ed3611a85540
Jacob Petrie <51274282+IZZY1996@users.noreply.github.com>
2022-03-08 10:00:19 -0600

    Update DataStreams.ps1

--- DataStreams.ps1
+++ DataStreams.ps1
@@ -7,13 +7,11 @@ $streams = (Get-ChildItem -Recurse | get-item -stream * | Where-Object {$_.strea
 
 foreach ($stream in $streams) {
     if ($stream.Stream -eq "SmartScreen"){ # if the stream is a smartscreen
+    
         $ww = (get-content $stream.pspath)
-        if ($ww -eq "Anaheim"){
-
-        }
-        else{
-            $ror += $stream
-        }
+        
+        if ($ww -eq "Anaheim"){}
+        else { $ror += $stream }
     }
     elseif ($stream.Stream -eq "Zone.Identifier"){ # if the stream is a MOTW
 
@@ -23,21 +21,16 @@ foreach ($stream in $streams) {
         elseif (($yy.Length -eq 2) -and ($yy[0] -eq "[ZoneTransfer]") -and ($yy[1] -match 'ZoneID=[1234]')) {}
         elseif (($yy.Length -eq 3) -and ($yy[0] -eq "[ZoneTransfer]") -and ($yy[1] -match 'LastWriterPackageFamilyName=+.') -and ($yy[2] -match 'ZoneID=[1234]')) {}
         elseif (($yy.Length -eq 4) -and ($yy[0] -eq "[ZoneTransfer]") -and ($yy[1] -match 'ZoneID=[1234]') -and ($yy[2] -match 'LastWriterPackageFamilyName=+.') -and ($yy[3] -match 'ZoneID=\d')) {}
-
-        else {
-            $ror += $stream 
-        }
+        else { $ror += $stream }
     }
     elseif ($stream.Stream -eq "Afp_AfpInfo") {
+    
         $ll = Get-Content -LiteralPath $stream.pspath.ToString()
+        
         if ($ll -eq 'AFP☺€PDF CARO') {}
-        else{
-            $ror += $stream
-        }
+        else { $ror += $stream }
     }
-    else { # if none of the above apply
-        $ror += $stream
-    }
+    else { $ror += $stream } # if none of the above apply
 }
 
 # write output
