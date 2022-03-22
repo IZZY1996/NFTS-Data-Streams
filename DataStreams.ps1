814b6a47a408543b312e43574e5c37edb9604513
Jacob Petrie <51274282+IZZY1996@users.noreply.github.com>
2022-03-22 00:19:12 -0500

    Cleaned the formating

--- DataStreams.ps1
+++ DataStreams.ps1
@@ -2,42 +2,44 @@
 #   Find Data Streams    #
 ##########################
 function Get-DataStream {
-$ror = @()
-$streams = (Get-ChildItem -Recurse | get-item -stream * | Where-Object {$_.stream -ne ':$Data'})
-
-foreach ($stream in $streams) {
-    if ($stream.Stream -eq "SmartScreen"){ # if the stream is a smartscreen
+    $ror = @()
+    $streams = (Get-ChildItem -Recurse | get-item -stream * | Where-Object { $_.stream -ne ':$Data' })
     
-        $ww = (get-content $stream.pspath)
+    foreach ($stream in $streams) {
+        if ($stream.Stream -eq "SmartScreen") {
+            # if the stream is a smartscreen
         
-        if ($ww -eq "Anaheim"){}
-        else { $ror += $stream }
-    }
-    elseif ($stream.Stream -eq "Zone.Identifier"){ # if the stream is a MOTW
-
-        $yy = Get-Content -LiteralPath $stream.pspath.ToString()
-
-        if (($yy.Length -eq 4 -or $yy.length -eq 3) -and ($yy[0] -eq "[ZoneTransfer]") -and ($yy[1] -match 'ZoneID=[1234]') -and ($yy[2] -match 'ReferrerUrl=+.' -or $yy[2] -match 'HostUrl=+.')){}
-        elseif (($yy.Length -eq 2) -and ($yy[0] -eq "[ZoneTransfer]") -and ($yy[1] -match 'ZoneID=[1234]')) {}
-        elseif (($yy.Length -eq 3) -and ($yy[0] -eq "[ZoneTransfer]") -and ($yy[1] -match 'LastWriterPackageFamilyName=+.') -and ($yy[2] -match 'ZoneID=[1234]')) {}
-        elseif (($yy.Length -eq 4) -and ($yy[0] -eq "[ZoneTransfer]") -and ($yy[1] -match 'ZoneID=[1234]') -and ($yy[2] -match 'LastWriterPackageFamilyName=+.') -and ($yy[3] -match 'ZoneID=\d')) {}
-        else { $ror += $stream }
-    }
-    elseif ($stream.Stream -eq "Afp_AfpInfo") {
+            $ww = (get-content $stream.pspath)
+            
+            if ($ww -eq "Anaheim") {}
+            else { $ror += $stream }
+        }
+        elseif ($stream.Stream -eq "Zone.Identifier") {
+            # if the stream is a MOTW
     
-        $ll = Get-Content -LiteralPath $stream.pspath.ToString()
+            $yy = Get-Content -LiteralPath $stream.pspath.ToString()
+    
+            if (($yy.Length -eq 4 -or $yy.length -eq 3) -and ($yy[0] -eq "[ZoneTransfer]") -and ($yy[1] -match 'ZoneID=[1234]') -and ($yy[2] -match 'ReferrerUrl=+.' -or $yy[2] -match 'HostUrl=+.')) {}
+            elseif (($yy.Length -eq 2) -and ($yy[0] -eq "[ZoneTransfer]") -and ($yy[1] -match 'ZoneID=[1234]')) {}
+            elseif (($yy.Length -eq 3) -and ($yy[0] -eq "[ZoneTransfer]") -and ($yy[1] -match 'LastWriterPackageFamilyName=+.') -and ($yy[2] -match 'ZoneID=[1234]')) {}
+            elseif (($yy.Length -eq 4) -and ($yy[0] -eq "[ZoneTransfer]") -and ($yy[1] -match 'ZoneID=[1234]') -and ($yy[2] -match 'LastWriterPackageFamilyName=+.') -and ($yy[3] -match 'ZoneID=\d')) {}
+            else { $ror += $stream }
+        }
+        elseif ($stream.Stream -eq "Afp_AfpInfo") {
         
-        if ($ll -eq 'AFP☺€PDF CARO') {}
-        else { $ror += $stream }
+            $ll = Get-Content -LiteralPath $stream.pspath.ToString()
+            
+            if ($ll -eq 'AFP☺€PDF CARO') {}
+            else { $ror += $stream }
+        }
+        else { $ror += $stream } # if none of the above apply
     }
-    else { $ror += $stream } # if none of the above apply
-}
-
-# write output
-"------------ Suspicious Data Streams ------------"
-ForEach ($s in $ror) {
-    $e,$r = $s.PSParentPath -split "::"
-    Write-Host "$r\" -ForegroundColor darkGray -NoNewline
-    Write-Host $s.PSChildName -ForegroundColor White
-}
+    
+    # write output
+    "------------ Suspicious Data Streams ------------"
+    ForEach ($s in $ror) {
+        $e, $r = $s.PSParentPath -split "::"
+        Write-Host "$r\" -ForegroundColor darkGray -NoNewline
+        Write-Host $s.PSChildName -ForegroundColor White
+    }
 }
