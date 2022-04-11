80752aa2c9dd47360425b9762857d06bec9848e1
Jacob Petrie <51274282+IZZY1996@users.noreply.github.com>
2022-04-11 10:45:57 -0500

    Update Security-Audit-Tool.psm1

--- Security-Audit-Tool.psm1
+++ Security-Audit-Tool.psm1
@@ -13,6 +13,32 @@ function Get-SATDataStream {
             https://docs.microsoft.com/en-us/openspecs/windows_protocols/ms-fscc/a82e9105-2405-4e37-b2c3-28c773902d85
 
             <Note:> A directory can also have an ADS
+
+            A table of all the standard attribute types
+            Attribute Name          Description
+            ---------------         ------------
+            $ATTRIBUTE_LIST         Lists the location of all attribute records that do not fit in the MFT record
+            $BITMAP                 Attribute for Bitmaps (Currently Used)
+            $DATA	                Contains the default file data (Currently Used)
+            $EA	                    Extended the attribute index
+            $EA_INFORMATION	        Extended attribute information
+            $FILE_NAME	            File name
+            $INDEX_ALLOCATION	    The type name for a Directory Stream. A string for the attribute code for index allocation (Currently Used)
+            $INDEX_ROOT	            Used to support folders and other indexes
+            $LOGGED_UTILITY_STREAM	Use by the encrypting file system
+            $OBJECT_ID	            Unique GUID for every MFT record
+            $PROPERTY_SET	        Obsolete
+            $REPARSE_POINT	        Used for volume mount points
+            $SECURITY_DESCRIPTOR	Security descriptor stores ACL and SIDs
+            $STANDARD_INFORMATION	Standard information, such as file times and quota data
+            $SYMBOLIC_LINK	        Obsolete
+            $TXF_DATA	            Transactional NTFS data
+            $VOLUME_INFORMATION	    Version and state of the volume
+            $VOLUME_NAME	        Name of the volume
+            $VOLUME_VERSION	        Obsolete. Volume version
+
+            https://docs.microsoft.com/en-us/openspecs/windows_protocols/ms-fscc/a82e9105-2405-4e37-b2c3-28c773902d85
+
             --------------------------------------------
     #>
     [CmdletBinding()]
